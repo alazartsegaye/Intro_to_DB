ec836f7b5246c2a8244b09e408f577cc2690e5f8
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-01-28 23:33:03 +0300

    Delete task_6.sql

--- task_6.sql
+++
@@ -1,8 +0,0 @@
--- Use the alx_book_store database
-USE alx_book_store;
-
--- Insert a single row into the Customers table
-INSERT INTO Customer (customer_id, customer_name, email, address)
-VALUES(2,'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-VALUES(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
-VALUES(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
